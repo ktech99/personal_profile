162399385096a8b15625162138718cdf741d5b4c
Kartik Arora <kartikarora99@gmail.com>
2018-08-31 12:09:10 +0530

    initial landing page with about

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function loadAbout(){
+  about = document.getElementById('about');
+  about.innerHTML = "I am a student at the University of Washington, intending to major in Computer-Science.<br><br>"
+  +"I love to work on projects, learn new things, and continue to improve myself constantly.";
+}
