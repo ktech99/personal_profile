a34f63f5d982a3841ef819046799b7707b10909d
Kartik Arora <kartikarora99@gmail.com>
2018-08-31 15:53:50 +0530

    remove dropdown from menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 function loadAbout(){
   about = document.getElementById('about');
+
   about.innerHTML = "I am a student at the University of Washington, intending to major in Computer-Science.<br><br>"
-  +"I love to work on projects, learn new things, and continue to improve myself constantly.";
+  +"I love to work on projects, learn new things, and continue to improve myself constantly.<br><br>"
+  +"I'll listen to your idea over coffee if you want to collaborate.<br> Black, no sugar.";
+}
+
+
+function pausecomp(millis)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < millis);
 }
