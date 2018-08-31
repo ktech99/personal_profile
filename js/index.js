e66ccbbf3190d1ec2bc53fe35b9cc0fc2e95ed4d
Kartik Arora <kartikarora99@gmail.com>
2018-08-31 16:14:35 +0530

    add skills

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 function loadAbout(){
   about = document.getElementById('about');
-
+  about.style.visible = "true";
   about.innerHTML = "I am a student at the University of Washington, intending to major in Computer-Science.<br><br>"
   +"I love to work on projects, learn new things, and continue to improve myself constantly.<br><br>"
   +"I'll listen to your idea over coffee if you want to collaborate.<br> Black, no sugar.";
 }
 
-
-function pausecomp(millis)
-{
-    var date = new Date();
-    var curDate = null;
-    do { curDate = new Date(); }
-    while(curDate-date < millis);
+function loadSkills(){
+  about = document.getElementById('about');
+  about.innerHTML = "<iframe src=\"skills.html\" frameBorder=\"0\" width=\"500\", height=\"500\"></iframe>";
 }
